ac9037b5f3be6f03b60f682eccb0b32525a33ef4
eoinmoney78 <94717367+eoinmoney78@users.noreply.github.com>
2022-11-30 14:15:20 -0500

    lecture notes update

---
+++ unit-2/variablesandconsole.js
@@ -0,0 +1,55 @@
+/*
+var 
+let 
+const 
+    -variables that hold data - doesn't change 
+(or none)
+*/
+
+let a = 2;
+let streetAddress = '123 main street';
+
+let b = 1;
+/*
+1. javascript keyword to denote the creation of a variable
+2.the name of the variable .
+-Developers refer to in order to grab the value stored.
+3.Assignment operator
+4. The initial value.
+*/
+// Variable Declaration
+let x;
+
+// Variable Initialization 
+let y = 10; // Anything on the right side of the assignment operator (=)
+
+// Assigning to Other Variables
+let first = 'john';
+let last = 'smith';
+let full = first + last; console.log('Declaration:', x);
+x = 10;
+console.log('Initialization 1: ', x);
+x = 33; // reassigning the value of our variable.
+console.log('Initialization 2: ', x);
+
+
+let hi = 'hello';
+console.log('Both: ', x, hi);
+// const = 'new' in ES6 declare variables meant to remain unchanged.
+let today = 'Great!';
+const javaScript = 'Wonderful';
+console.log(today, javaScript);
+today = 'Lovely!';
+console.log(today, javaScript);
+eric = 'Eric';
+console.log(eric);
+let eoin = 'huo';
+console.log(eoin);
+console.warn('this is a warning');
+let warning = 'Uh oh';
+console.log('warn:', warning);
+let err = 'this looks real bad...';
+console.error('error: ', err);
+
+
+
