ecc2c3c1c8770908d6b967df4ae6a69d9d49f526
eoinmoney78 <94717367+eoinmoney78@users.noreply.github.com>
2023-02-01 20:32:35 -0500

    unit-6-2

--- unit-6/movies-mongodb/models/user.model.js
+++ unit-6/movies-mongodb/models/user.model.js
@@ -17,8 +17,8 @@ const UserSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true,
-    },
+        required: true
+    }
     // passwordCheck: String
 });
 
